84257b3bbc9319c1e42a2f6a734533d3214a1808
bin.xue <bin.xue@alibaba-inc.com>
2023-03-06 10:28:34 +0800

    support model from standalone training

--- print_model.py
+++ print_model.py
@@ -7,5 +7,9 @@ import torch
 from modelscope.models.audio.kws.farfield.fsmn_sele_v2 import FSMNSeleNetV2
 
 model = torch.load(sys.argv[1], map_location='cpu')
-model.print_header()
-model.print_model()
+if hasattr(model, 'print_header'):
+    model.print_header()
+    model.print_model()
+else:
+    model.printHeader()
+    model.printModel()
